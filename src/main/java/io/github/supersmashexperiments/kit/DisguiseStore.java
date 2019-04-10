babfa46df7d9ffd2c0f2fb561429789c5ae46fc2
George Bakewell <sbubway@gmail.com>
2019-04-10 16:20:53 +0100

    Create the KitStore and DisguiseStore interfaces

---
+++ src/main/java/io/github/supersmashexperiments/kit/DisguiseStore.java
@@ -0,0 +1,7 @@
+package io.github.supersmashexperiments.kit;
+
+import me.libraryaddict.disguise.disguisetypes.Disguise;
+
+public interface DisguiseStore {
+  public Disguise get(String disguiseId);
+}
---
+++ src/main/java/io/github/supersmashexperiments/kit/KitStore.java
@@ -0,0 +1,7 @@
+package io.github.supersmashexperiments.kit;
+
+import io.github.supersmashexperiments.kit.SuperSmashKit;
+
+public interface KitStore {
+  public SuperSmashKit get(Tag tag);
+}

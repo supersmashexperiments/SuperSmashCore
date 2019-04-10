ae9ca971036757753675c2120a58f8436c4a82d4
George Bakewell <sbubway@gmail.com>
2019-04-10 17:49:31 +0100

    Make EventHandler implement Map.Entry

--- src/main/java/io/github/supersmashexperiments/kit/EventHandler.java
+++ src/main/java/io/github/supersmashexperiments/kit/EventHandler.java
@@ -1,15 +1,25 @@
 package io.github.supersmashexperiments.kit;
 
+import java.util.AbstractMap;
+import java.util.Map;
+
 import org.bukkit.event.Event;
 import org.bukkit.plugin.EventExecutor;
 
-public class EventHandler {
+public class EventHandler extends AbstractMap.SimpleImmutableEntry<Class<? extends Event>, EventExecutor> {
   private final Class<? extends Event> eventClass;
   private final EventExecutor executor;
 
   public EventHandler(Class<? extends Event> eventClass, EventExecutor executor) {
+    super(eventClass, executor);
     this.eventClass = eventClass;
     this.executor = executor;
+  }
+
+  public EventHandler(Map.Entry<Class<? extends Event>, EventExecutor> tuple) {
+    super(tuple);
+    this.eventClass = tuple.getKey();
+    this.executor = tuple.getValue();
   }
 
   public Class<? extends Event> getEventClass() {

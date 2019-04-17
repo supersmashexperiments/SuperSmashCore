a1642ed8008c5a695bbd2e5dbe1539cccc5f9f15
George Bakewell <sbubway@gmail.com>
2019-04-17 02:19:52 +0100

    Remove Tag class from core
    I feel this is a step towards better separating implementation and
    specification.
    
    The core need only know about SuperSmashKit, not how it is
    stored/identified by its implementors.

--- src/main/java/io/github/supersmashexperiments/ssc/kit/DisguiseStore.java
+++
@@ -1,8 +0,0 @@
-package io.github.supersmashexperiments.ssc.kit;
-
-import io.github.supersmashexperiments.ssc.kit.exception.DisguiseNotFoundException;
-import me.libraryaddict.disguise.disguisetypes.Disguise;
-
-public interface DisguiseStore {
-  public Disguise get(String disguiseId) throws DisguiseNotFoundException;
-}
--- src/main/java/io/github/supersmashexperiments/ssc/kit/KitStore.java
+++
@@ -1,8 +0,0 @@
-package io.github.supersmashexperiments.ssc.kit;
-
-import io.github.supersmashexperiments.ssc.kit.SuperSmashKit;
-import io.github.supersmashexperiments.ssc.kit.exception.KitNotFoundException;
-
-public interface KitStore {
-  public SuperSmashKit get(Tag tag) throws KitNotFoundException;
-}

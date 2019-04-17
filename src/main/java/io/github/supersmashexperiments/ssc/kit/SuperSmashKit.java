923a6152ec301eeac09c6b45ac49116d0fdc77c4
George Bakewell <sbubway@gmail.com>
2019-04-17 02:18:51 +0100

    Add name and description fields to SuperSmashKit

--- src/main/java/io/github/supersmashexperiments/ssc/kit/SuperSmashKit.java
+++ src/main/java/io/github/supersmashexperiments/ssc/kit/SuperSmashKit.java
@@ -1,12 +1,24 @@
 package io.github.supersmashexperiments.ssc.kit;
 
 public final class SuperSmashKit {
+  private final String name;
+  private final String description;
   private final Abilities abilities;
   private final Armor armor;
 
-  public SuperSmashKit(Abilities abilities, Armor armor) {
+  public SuperSmashKit(String name, String description, Abilities abilities, Armor armor) {
+    this.name = name;
+    this.description = description;
     this.armor = armor;
     this.abilities = abilities;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getDescription() {
+    return this.description;
   }
 
   public Abilities getAbilities() {

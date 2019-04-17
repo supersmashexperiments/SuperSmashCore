1b525b929421498f85cf7d584a270022214bd235
George Bakewell <sbubway@gmail.com>
2019-04-17 15:39:10 +0100

    Add dekit method to remove kits from players

--- src/main/java/io/github/supersmashexperiments/ssc/Controller.java
+++ src/main/java/io/github/supersmashexperiments/ssc/Controller.java
@@ -1,5 +1,7 @@
 package io.github.supersmashexperiments.ssc;
 
+import java.util.HashMap;
+
 import org.bukkit.entity.Player;
 
 import io.github.supersmashexperiments.ssc.event.SmashEventHandler;
@@ -7,6 +9,7 @@ import io.github.supersmashexperiments.ssc.kit.*;
 
 public class Controller {
   private static final EventRegistrar events = new EventRegistrar();
+  private static final HashMap<Player, SuperSmashKit> players = new HashMap<>();
   private final Plugin plugin;
 
   Controller(Plugin plugin) {
@@ -16,6 +19,8 @@ public class Controller {
   }
 
   public static void equip(Player player, SuperSmashKit kit) {
+    players.put(player, kit);
+
     PlayerStateManager.store(player);
 
     for (Ability ability : kit.getAbilities()) {
@@ -23,6 +28,23 @@ public class Controller {
 
       for (int i = 0; i < eventHandlers.length; i++) {
         events.registerEventHandler(eventHandlers[i]);
+      }
+    }
+  }
+
+  public static void dekit(Player player) {
+    SuperSmashKit kit = players.get(player);
+
+    if (kit == null)
+      return;
+
+    PlayerStateManager.restore(player);
+
+    for (Ability ability : kit.getAbilities()) {
+      SmashEventHandler[] eventHandlers = ability.getEventHandlers();
+
+      for (int i = 0; i < eventHandlers.length; i++) {
+        events.unregisterEventHandler(eventHandlers[i]);
       }
     }
   }
